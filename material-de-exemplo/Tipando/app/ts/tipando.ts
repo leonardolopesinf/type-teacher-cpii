1f9e168971cc412f85ffddeb420bba60aa85ee2b
viniciusfoc <52377932+viniciusfoc@users.noreply.github.com>
2019-07-12 10:37:04 -0300

    tipando
    exemplificando como tipar variaveis

---
+++ material-de-exemplo/Tipando/app/ts/tipando.ts
@@ -0,0 +1,11 @@
+//Primeiro temos que escolher se usamos "var" ou "let" e um nome pra variável
+// Vamos escolher uma variável com nome de idade , e vamos usar o let que é mais comum
+// Com a variável já declarada vamos escolher um tipo, como pegamos idade vamos colocar number
+// Com o tipo number já podemos atribuir um valor a variável que aceito o tipo number , no caso são números
+let idade : number = 1958;
+// Vamos escolher mais um para escolher um tipo para fixar
+// Vamos fazer uma lista então uma boa opção é o tipo array
+let lista : number[] = [1, 2, 3];
+// Agora podemos colocar para aparecer essas variáveis para termos certeza que funciona.
+document.write(JSON.stringify(idade)+'br')
+document.write(JSON.stringify(lista)+'br')
