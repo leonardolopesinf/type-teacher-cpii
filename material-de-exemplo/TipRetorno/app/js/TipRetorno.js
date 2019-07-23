3e90463c37035168d605ff9e8682530d6d3b02a7
Filipe Christovão <52300819+FilipeAC16@users.noreply.github.com>
2019-07-22 21:45:01 -0300

    adicionando Tipando Retorno

---
+++ material-de-exemplo/TipRetorno/app/js/TipRetorno.js
@@ -0,0 +1,6 @@
+//abaixo, criaremos uma função simples para exemplificar
+//a tipagem de retorno é feita de maneira simples, apenas usando '=>'
+function exemplo(x, y) {
+    return x + y;
+}
+let exemplo1; //aqui, tipamos o retorno como number usando '=>'
