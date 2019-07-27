b478a1656b9415acc1cf62bb6a68c4c126fb532e
Filipe Christovão <52300819+FilipeAC16@users.noreply.github.com>
2019-07-27 10:26:37 -0300

    adicionando Enum

--- material-de-exemplo/Tipagem/Tbasicos/app/ts/tiposbasicos.ts
+++ material-de-exemplo/Tipagem/Tbasicos/app/ts/tiposbasicos.ts
@@ -5,6 +5,7 @@ let tuple: [string,number] = ['bom dia', 7];
 let n: null = null;
 let array: number[] = [1, 2, 3]; // também pode ser (let lista: Array<number> = [1, 2, 3];)
 let algo: any = 4;
+let x: enum { A = 1, B, C }
 
 // Todos passaremos para string para podermos visualizar-los no index.html
 
@@ -15,3 +16,4 @@ document.write('Tuple --> ' + JSON.stringify(tuple) + '<br>');
 document.write('Null --> ' + JSON.stringify(n) + '<br>');
 document.write('Array --> ' + JSON.stringify(array) + '<br>');
 document.write('Any --> ' + JSON.stringify(algo) + '<br>');
+document.write('Enum --> ' + JSON.stringify(x) + '<br>');
