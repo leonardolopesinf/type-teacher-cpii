e77a378ae20e27485e26d148d6bc555b699fe02b
Leonardo Henrique <leonardolopesinf@gmail.com>
2019-07-29 01:47:43 -0300

    Correção Enum

--- material-de-exemplo/Tipagem/Tbasicos/app/js/tiposbasicos.js
+++ material-de-exemplo/Tipagem/Tbasicos/app/js/tiposbasicos.js
@@ -5,6 +5,12 @@ let tuple = ['bom dia', 7];
 let n = null;
 let array = [1, 2, 3]; // também pode ser (let lista: Array<number> = [1, 2, 3];)
 let algo = 4;
+var Enum;
+(function (Enum) {
+    Enum[Enum["A"] = 0] = "A";
+    Enum[Enum["B"] = 1] = "B";
+    Enum[Enum["C"] = 2] = "C";
+})(Enum || (Enum = {}));
 // Todos passaremos para string para podermos visualizar-los no index.html
 document.write('Boolean --> ' + JSON.stringify(booleano) + '<br>');
 document.write('Number --> ' + JSON.stringify(numero) + '<br>');
@@ -12,4 +18,11 @@ document.write('String --> ' + JSON.stringify(texto) + '<br>');
 document.write('Tuple --> ' + JSON.stringify(tuple) + '<br>');
 document.write('Null --> ' + JSON.stringify(n) + '<br>');
 document.write('Array --> ' + JSON.stringify(array) + '<br>');
-document.write('Any --> ' + JSON.stringify(algo) + '<br>');
+document.write('Any --> ' + JSON.stringify(algo) + '<br><br>');
+document.write('Enum --> ' + JSON.stringify(Enum) + '<br>');
+document.write('Enum.A --> ' + JSON.stringify(Enum.A) + '<br>');
+document.write('Enum.B --> ' + JSON.stringify(Enum.B) + '<br>');
+document.write('Enum.C --> ' + JSON.stringify(Enum.C) + '<br>');
+document.write('Enum[0] --> ' + JSON.stringify(Enum[0]) + '<br>');
+document.write('Enum[1] --> ' + JSON.stringify(Enum[1]) + '<br>');
+document.write('Enum[2] --> ' + JSON.stringify(Enum[2]) + '<br>');
--- material-de-exemplo/Tipagem/Tbasicos/app/ts/tiposbasicos.ts
+++ material-de-exemplo/Tipagem/Tbasicos/app/ts/tiposbasicos.ts
@@ -5,7 +5,11 @@ let tuple: [string,number] = ['bom dia', 7];
 let n: null = null;
 let array: number[] = [1, 2, 3]; // também pode ser (let lista: Array<number> = [1, 2, 3];)
 let algo: any = 4;
-let x: enum { A = 1, B, C }
+enum Enum{
+    A = 0,
+    B = 1,
+    C = 2
+}
 
 // Todos passaremos para string para podermos visualizar-los no index.html
 
@@ -15,5 +19,12 @@ document.write('String --> ' + JSON.stringify(texto) + '<br>');
 document.write('Tuple --> ' + JSON.stringify(tuple) + '<br>');
 document.write('Null --> ' + JSON.stringify(n) + '<br>');
 document.write('Array --> ' + JSON.stringify(array) + '<br>');
-document.write('Any --> ' + JSON.stringify(algo) + '<br>');
-document.write('Enum --> ' + JSON.stringify(x) + '<br>');
+document.write('Any --> ' + JSON.stringify(algo) + '<br><br>');
+
+document.write('Enum --> ' + JSON.stringify(Enum) + '<br>');
+document.write('Enum.A --> ' + JSON.stringify(Enum.A) + '<br>');
+document.write('Enum.B --> ' + JSON.stringify(Enum.B) + '<br>');
+document.write('Enum.C --> ' + JSON.stringify(Enum.C) + '<br>');
+document.write('Enum[0] --> ' + JSON.stringify(Enum[0]) + '<br>');
+document.write('Enum[1] --> ' + JSON.stringify(Enum[1]) + '<br>');
+document.write('Enum[2] --> ' + JSON.stringify(Enum[2]) + '<br>');
