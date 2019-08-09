f17b4beec23ccb13f09fb61f212711cf3df47f85
Lara Beatriz <43223887+Larabms@users.noreply.github.com>
2019-08-09 18:55:25 -0300

    adicionando declaração de types

---
+++ material-de-exemplo/Tipagem/declarartypes/ts/teste.ts
@@ -0,0 +1,9 @@
+let nome: string = "Jorge"; 
+let pontuacao1: number = 47; 
+let pontuacao2: number = 52.50 
+let sum = pontuacao1 + pontuacao2 
+console.log("nome"+nome) // Jorge
+console.log("primeira pontuação: "+ pontuacao1) // 47
+console.log("segunda pontuação: "+pontuacao2) // 52.50
+console.log("soma das pontuações: "+sum) // 99.50
+
