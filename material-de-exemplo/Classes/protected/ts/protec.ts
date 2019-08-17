e879a819128253d6f0d67e2e09b30dc6b07391be
Lara Beatriz <43223887+Larabms@users.noreply.github.com>
2019-08-17 12:26:33 -0300

    Adicionando protected

---
+++ material-de-exemplo/Classes/protected/ts/protec.ts
@@ -0,0 +1,24 @@
+class Person {
+    protected name:string;
+    constructor(name: string) { this.name = name; }
+}
+
+class Employee extends Person {
+    private department: string;
+
+    constructor(name: string, department: string) {
+        super(name);
+        this.department = department;
+    }
+
+    public getElevatorPitch() {
+        return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+    }
+}
+
+let howard = new Employee("Howard", "Sales");
+console.log(howard.getElevatorPitch());
+console.log(howard.name); // Erro
+
+
+//Embora não possamos usar o nome de fora da pessoa, ainda podemos usá-lo de dentro de um método de instância do funcionário porque o funcionário deriva da pessoa
