9f26eb03b48704020e9093c4ccfc578e55dd9db7
Lara Beatriz <43223887+Larabms@users.noreply.github.com>
2019-08-17 12:36:44 -0300

    adicionando private

---
+++ material-de-exemplo/Classes/private/ts/priv.ts
@@ -0,0 +1,22 @@
+class Animal {
+    private name: string;
+    constructor(theName: string) { this.name = theName; }
+}
+
+class Rhino extends Animal {
+    constructor() { super("Rhino"); }
+}
+
+class Employee {
+    private name: string;
+    constructor(theName: string) { this.name = theName; }
+}
+
+let animal = new Animal("Goat");
+let rhino = new Rhino();
+let employee = new Employee("Bob");
+
+animal = rhino;
+animal = employee; // Error: 'Animal' and 'Employee' are not compatible
+
+// Como Animal e Rhino compartilham o lado privado de sua forma da mesma declaração de name private: string em Animal, eles são compatíveis. No entanto, esse não é o caso do Employee
