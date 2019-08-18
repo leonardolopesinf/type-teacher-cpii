692889b230fe939a2ed002c45dc515998c7b6e74
Leonardo Lopes <leonardolopesinf@gmail.com>
2019-08-18 13:33:13 -0300

    Removendo public em local errado

--- material-de-exemplo/public/ts/public.ts
+++
@@ -1,7 +0,0 @@
-class Animal {
-    public name: string;
-    public constructor(theName: string) { this.name = theName; }
-    public move(distanceInMeters: number) {
-        console.log(`${this.name} moved ${distanceInMeters}m.`);
-    }
-}
